63428f6a368b0f97dd8dc9dc0f2dea42cd0525e9
brian <brianmazini@gmail.com>
2020-09-20 21:55:50 -0300

    Gravando categoria. Refatorar CreateTransactionService para dividir responsabilidade

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,43 @@
+import { getCustomRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import CategorysRepository from '../repositories/CategorysRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: RequestDTO): Promise<Transaction> {
+    const categorysRepository = getCustomRepository(CategorysRepository);
+
+    const newCategory = categorysRepository.create({
+      title: category,
+    });
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: newCategory.id,
+    });
+
+    await categorysRepository.save(newCategory);
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
