983a884226a61e49ece410333e1519d50d3adc90
brian <brianmazini@gmail.com>
2020-09-23 23:24:03 -0300

    Verificando categoria existente

--- src/repositories/CategorysRepository.ts
+++ src/repositories/CategorysRepository.ts
@@ -3,6 +3,14 @@ import { EntityRepository, Repository } from 'typeorm';
 import Category from '../models/Category';
 
 @EntityRepository(Category)
-class CategorysRepository extends Repository<Category> {}
+class CategorysRepository extends Repository<Category> {
+  public async findByTitle(title: string): Promise<Category | undefined> {
+    const findedCategory = await this.findOne({
+      where: { title },
+    });
+
+    return findedCategory;
+  }
+}
 
 export default CategorysRepository;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -20,21 +20,23 @@ class CreateTransactionService {
     category,
   }: RequestDTO): Promise<Transaction> {
     const categorysRepository = getCustomRepository(CategorysRepository);
-
-    const newCategory = categorysRepository.create({
-      title: category,
-    });
+    const categoryFinded = await categorysRepository.findByTitle(category).then(
+      x =>
+        x ||
+        categorysRepository.save(
+          categorysRepository.create({
+            title: category,
+          }),
+        ),
+    );
 
     const transactionsRepository = getCustomRepository(TransactionsRepository);
-
     const transaction = transactionsRepository.create({
       title,
       value,
       type,
-      category_id: newCategory.id,
+      category_id: categoryFinded.id,
     });
-
-    await categorysRepository.save(newCategory);
     await transactionsRepository.save(transaction);
 
     return transaction;
