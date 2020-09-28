a6fca2a45ba4d1fd896cabba589bce4d9eda3c89
brian <brianmazini@gmail.com>
2020-09-27 21:09:13 -0300

    Import incompleto

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,8 +1,17 @@
 import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
+import csvParse from 'csv-parse';
+import fs from 'fs';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+interface ResponseFileDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 export default {
   directory: tmpFolder,
@@ -15,4 +24,30 @@ export default {
       return callback(null, fileName);
     },
   }),
+
+  loadCsv: async (fileName: string): Promise<ResponseFileDTO[]> => {
+    const csvFilePath = path.resolve(tmpFolder, fileName);
+
+    const readCSVStream = fs.createReadStream(csvFilePath);
+
+    const parseStream = csvParse({
+      from_line: 2,
+      ltrim: true,
+      rtrim: true,
+    });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    const lines: ResponseFileDTO[] = [];
+
+    parseCSV.on('data', line => {
+      lines.push(line);
+    });
+
+    await new Promise(resolve => {
+      parseCSV.on('end', resolve);
+    });
+
+    return lines;
+  },
 };
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,6 +1,7 @@
 import { getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
 import TransactionsRepository from '../repositories/TransactionsRepository';
+import CategorysRepository from '../repositories/CategorysRepository';
 import upload from '../config/upload';
 
 interface RequestDTO {
@@ -12,13 +13,91 @@ interface RequestDTO {
 
 class ImportTransactionsService {
   async execute(filename: string): Promise<Transaction[]> {
-    console.log(upload.directory);
+    const data = await upload.loadCsv(filename);
 
     const transactionsRepository = getCustomRepository(TransactionsRepository);
+    data.map(t => {
+      console.log(t);
+      console.log(t.title);
+      console.log(t.value);
+      console.log(t.type);
+      console.log(t.category);
+    });
+
+    // data.map(t => this.save(t));
+
+    // return data;
+
+    // const transactionsRepository = getCustomRepository(TransactionsRepository);
     const transactions = transactionsRepository.find();
 
     return transactions;
   }
+
+  public async save({
+    title,
+    value,
+    type,
+    category,
+  }: RequestDTO): Promise<Transaction> {
+    // console.log(title);
+    // console.log(value);
+    // console.log(type);
+    // console.log(category);
+
+    const categorysRepository = getCustomRepository(CategorysRepository);
+    const categoryFinded = await categorysRepository.findByTitle(category).then(
+      x =>
+        x ||
+        categorysRepository.save(
+          categorysRepository.create({
+            title: category,
+          }),
+        ),
+    );
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryFinded.id,
+    });
+    await transactionsRepository.save(transaction);
+
+    return transaction;
+  }
+
+  // public saveAll(transactions: RequestDTO[]): Transaction[] {
+  //   const response = [] as Transaction[];
+
+  //   transactions.map(t => {
+  //     const categorysRepository = getCustomRepository(CategorysRepository);
+  //     const categoryFinded = await categorysRepository
+  //       .findByTitle(t.category)
+  //       .then(
+  //         x =>
+  //           x ||
+  //           categorysRepository.save(
+  //             categorysRepository.create({
+  //               title: t.category,
+  //             }),
+  //           ),
+  //       );
+
+  //     const transactionsRepository = getCustomRepository(
+  //       TransactionsRepository,
+  //     );
+  //     const transaction = transactionsRepository.create({
+  //       title: t.title,
+  //       value: t.value,
+  //       type: t.type,
+  //       category_id: categoryFinded.id,
+  //     });
+
+  //     return transactionsRepository.save(transaction);
+  //   });
+  // }
 }
 
 export default ImportTransactionsService;
